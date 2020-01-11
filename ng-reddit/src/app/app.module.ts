d2cae346897d42229753ef8d9e90d55d796e3b1c
Foodie <olaorefouad565@gmail.com>
2020-01-11 22:35:26 +0100

    began working on the UI, finished the header component and form

--- ng-reddit/src/app/app.module.ts
+++ ng-reddit/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArticleComponent } from './components/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
