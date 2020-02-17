9e1bcca2e6676b68f7a85f48f8530ed1b95db86a
Foodie <olaorefouad565@gmail.com>
2020-02-17 18:18:58 +0100

    deploy: dealt a lot with forms, controls, groups and also validators - both built-in and custom

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, AbstractControl, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forms';
+
+  myForm: FormGroup;
+  sku: AbstractControl;
+  formSubmitted = false;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      sku: ['', Validators.compose([
+        Validators.required, Validators.minLength(3), this.beginsWithMoney
+      ])]
+    });
+
+    this.sku = this.myForm.get('sku');
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+  }
+
+  // Custom Validator: Checks if the text starts with money
+  beginsWithMoney(control: FormControl): { [s: string]: boolean } {
+    const value = (control.value as string).toLowerCase();
+    if (!value.startsWith('money')) {
+      return { beginsWithMoney: true };
+    } else {
+      return null;
+    }
+  }
 }
